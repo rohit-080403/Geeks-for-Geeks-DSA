8f74f66c285c15b39ce3999a2daebf6150414db0
Rohit Kamble <rohitkamble0343@gmail.com>
2026-08-31 19:10:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest in Array/largest-in-array.py
+++ Difficulty: Basic/Largest in Array/largest-in-array.py
@@ -1,6 +1,11 @@
 class Solution:
     def largest(self, arr):
         # code here
-        arr.sort()
-        return arr[-1]
+        max_num = arr[0]
+        
+        for num in arr:
+            if num > max_num:
+                max_num = num
+        return max_num
+                
         
